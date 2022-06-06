89fb84bad87f6b4b505dfacc81f1293599eb14ca
kodowiec <62697517+kodowiec@users.noreply.github.com>
2022-06-06 22:54:13 +0200

    Initial commit

---
+++ krecikthegame/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace krecikthegame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
