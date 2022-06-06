79a0412679075562014098cdc44d2eaadc6bf9a1
kodowiec <62697517+kodowiec@users.noreply.github.com>
2022-06-07 01:18:25 +0200

    KCU: Characters: add box drawing characters

--- Krecik Console Utilities/Characters.cs
+++ Krecik Console Utilities/Characters.cs
@@ -3,5 +3,47 @@
     public class Characters
     {
         public static readonly char whitespace = ' ';
+
+        /// <summary>
+        ///<para>╔══╦══╗</para>
+        ///<para>╠══╬══╣</para>
+        ///<para>╚══╩══╝</para>
+        /// </summary>
+        public class BoxDrawing
+        {
+            /// <summary>
+            ///<para>╔ ╗</para>
+            ///<para>╚ ╝</para>
+            /// </summary>
+            public class Corners
+            {
+                public static readonly char TopLeft = '╔',
+                                            TopRight = '╗',
+                                            BottomLeft = '╚',
+                                            BottomRight = '╝';
+            }
+
+            /// <summary>
+            ///<para> ══╦══ </para>
+            ///<para> ══╩══ </para>
+            ///<para>╠══╬══╣</para>
+            /// </summary>
+            public class Walls
+            {
+            //  ╔══╦══╗
+            //  ║  ║  ║ 
+            //  ╠══╬══╣
+            //  ║  ║  ║
+            //  ╚══╩══╝
+                public static readonly char Vertical = '║',
+                                            VerticalRightEntrance = '╠',
+                                            VerticalLeftEntrance = '╣',
+                                            VerticalTwoEntrances = '╬';
+
+                public static readonly char Horizontal = '═',
+                                            HorizontalBottomEntrance = '╦',
+                                            HorizontalTopEntrance = '╩';
+            }
+        }
     }
 }
