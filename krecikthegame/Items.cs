f3da69a56f4e57288764d8dd777ca7c7fde20d3f
Natalia Nalewajk <natalia.karate@o2.pl>
2022-06-16 23:44:35 +0200

    Add basic item classes

---
+++ krecikthegame/Items.cs
@@ -0,0 +1,52 @@
+﻿namespace Gameplay
+{
+    enum ItemType
+    {
+        RED_POTION,
+        GOLD_KEY,
+        // ITP ITD
+    }
+
+    class Item: IEquatable<Item>
+    {
+        public ItemType Type;
+        public string Name;
+        public string Description;
+
+
+        public bool Equals(Item? other)
+        {   
+            if(other == null) return false;
+            return Type == other.Type;
+        }
+    }
+
+    class UsableItem: Item
+    {
+        public bool CanUse = true;
+
+        public virtual void Use() { }
+    }
+
+    // ITEMS
+
+    // EXAMPLE ////////////////////////////
+    class RedPotion: UsableItem
+    {
+        float HP = 30.0f;
+
+        public RedPotion() 
+        {
+            Type = ItemType.RED_POTION;
+            Name = "Red Potion";
+            Description = "Restores some HP points";
+        }
+
+        public override void Use() 
+        {
+            Console.WriteLine("+30hp");
+        }
+    }
+
+    
+}
