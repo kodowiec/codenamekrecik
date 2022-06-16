274b82a3085dd431f93229ffae98df69f8b1f676
Natalia Nalewajk <natalia.karate@o2.pl>
2022-06-16 23:45:28 +0200

    Add player basic classes

---
+++ krecikthegame/Player.cs
@@ -0,0 +1,33 @@
+﻿namespace Gameplay
+{
+    class Player
+    {
+        float MaxHP = 100;
+        float CurrentHP = 100;
+        public Equipment EQ = new Equipment();
+    }
+
+    class Equipment
+    {
+        List<Item> Items = new List<Item>();
+
+        public void AddItem(Item item)
+        {
+            Items.Add(item);
+        }
+
+        public void RemoveItem(ItemType itemType)
+        {
+            Item item = Items.Find(x => x.Type == itemType);
+            if (item != null)
+                Items.Remove(item);
+        }
+
+        public bool HasItem(ItemType itemType)
+        {
+            return Items.Exists(x => x.Type == itemType);
+        }
+
+        public List<Item> GetAllItems() { return Items; }
+    }
+}
