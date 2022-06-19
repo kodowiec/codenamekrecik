76128363b4116eab6b84a1e55a2e0ac11e0c6970
kodowiec <62697517+kodowiec@users.noreply.github.com>
2022-06-20 00:26:17 +0200

    Game: create MapObject class
    for "objects" that will be used to build the game world

---
+++ krecikthegame/Models/MapObject.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace krecikthegame.Models
+{
+    public class MapObject
+    {
+        public int ID { get; set; }
+        public string ColorID { get; set; }
+        public string Name { get; set; }
+        public string Display { get; set; }
+        public bool IsInteractive { get; set; }
+        public bool IsColliding { get; set; }
+        public ConsoleColor DrawColor { get; set; }
+    }
+}
