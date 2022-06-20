2111894d7834d74c67ccb329252ff1e85666f6b9
natsiii <natalia.karate@o2.pl>
2022-06-21 00:15:41 +0200

    Refactor player classes

---
+++ krecikthegame/Gameplay/Player/Equipment.cs
@@ -0,0 +1,31 @@
+﻿namespace Gameplay
+{
+    class Equipment
+    {
+        List<Item> Items = new List<Item>();
+
+        public void AddItem(Item item)
+        {
+            Items.Add(item);
+        }
+
+        public void RemoveItem(ItemType itemType)
+        {
+            Item item = Items.Find(x => x.Type == itemType);
+            if (item != null)
+                Items.Remove(item);
+        }
+
+        public bool HasItem(ItemType itemType)
+        {
+            return Items.Exists(x => x.Type == itemType);
+        }
+
+        public int ItemCount(ItemType itemType)
+        {
+            return Items.Where(x => x.Type == itemType).Count();
+        }
+
+        public List<Item> GetAllItems() { return Items; }
+    }
+}
